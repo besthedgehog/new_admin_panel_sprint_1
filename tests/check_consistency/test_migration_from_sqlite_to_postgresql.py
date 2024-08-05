0f9fd9da160c58afe62549ee30f759b336b47fc8
Alexey Polukhin <alexpolukhin@yandex.ru>
2024-08-05 09:42:28 +0300

    Вроде всё заработало

--- tests/check_consistency/test_migration_from_sqlite_to_postgresql.py
+++ tests/check_consistency/test_migration_from_sqlite_to_postgresql.py
@@ -15,9 +15,34 @@ from sqlite_to_postgres.migration_from_sqlite_to_postgresql \
 import get_all_information_from_sql as get_all_information_from_sqlite
 
 
+# def get_all_information_from_sqlite(conn):
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+#     all_tables_in_sqlite = tuple(i[0] for i in cursor.fetchall())
+#     print(all_tables_in_sqlite)
+
+#     sqlite_command = '''
+#     SELECT * from {};
+#     '''
+
+#     all_the_data_from_sqlite = dict()
+
+#     for table in all_tables_in_sqlite:
+#         cursor.execute(sqlite_command.format(table))
+#         all_the_data_from_the_table = cursor.fetchall()
+#         print(all_the_data_from_the_table)
+#         sys.exit(3)
+
+
+
+# def transform_datetime(dt: datetime) -> str:
+#     formatted_datetime = dt.strftime('%Y-%m-%d %H:%M:%S.%f%z')
+#     return formatted_datetime
+
 def transform_datetime(dt: datetime) -> str:
-    formatted_datetime = dt.strftime('%Y-%m-%d %H:%M:%S.%f%z')
-    return formatted_datetime
+    formatted_datetime = dt.strftime('%Y-%m-%d %H:%M:%S.%f')
+    formatted_timezone = dt.strftime('%z')[:3]  # Take only the first three characters of the timezone
+    return f"{formatted_datetime}{formatted_timezone}"
 
 
 def get_all_information_from_postgre(conn):
@@ -50,21 +75,26 @@ def get_all_information_from_postgre(conn):
     sql_comamnd = '''
         SELECT * from {};
     '''
-    list_with_data = list()
+
 
     for table in list_with_necessary_tables:
+        #### Исправить
+        # table = 'genre'
         cursor.execute(sql_comamnd.format(table))
 
-        # Список со словарями [{id: ..., type: ..., updated_at: ...}]
-        data_from_table = cursor.fetchall()
+        # Список со словарями
+        # Для таблицы genre
+        # [{id: ..., name: ..., description: ..., created_at, updated_at: ...}]
+        data_from_table = cursor.fetchall() # Вся инормация из одной таблицы
 
         # print(f'data_from_table = {data_from_table[0]}')
-        # sys.exit(3)
 
+        list_with_data = list()
+
+        # итерируемся по каждому словарю
         for dict_with_data in data_from_table:
-
             tmp_list = list() # Нужен для приведения к стуркутре как в Postgre
-
+            # приводим формат записи даты к нужному нам
             for element in tuple(dict_with_data.values()):
                 if isinstance(element, datetime):
                     tmp_list.append(transform_datetime(element))
@@ -77,6 +107,10 @@ def get_all_information_from_postgre(conn):
 
         data[table] = list_with_data
 
+
+    # print(data.keys())
+    # print(data['genre'])
+    # print(data)
     return data
 
 
@@ -104,6 +138,7 @@ def main():
         data_from_postgre = get_all_information_from_postgre(pg_conn)
 
 
+
         print('from sqlite')
         print(
             data_from_sqlite['genre'][1][0]
@@ -112,6 +147,10 @@ def main():
         print()
 
         print('from postgre')
+
+        # for i in data_from_postgre['genre'][0]:
+        #     print(i)
+        #     print()
         print(
             data_from_postgre['genre'][0]
         )
