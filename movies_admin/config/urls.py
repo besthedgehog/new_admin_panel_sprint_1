9319778bb9e673b886474552eb6898b9bcd6123f
Alexey Polukhin <alexpolukhin@yandex.ru>
2024-08-11 16:40:42 +0300

    Удалены комментарии

--- movies_admin/config/urls.py
+++ movies_admin/config/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 
 from django.contrib import admin
-# from django.urls import path
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
 
